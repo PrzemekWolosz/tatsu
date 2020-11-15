f533c254274273b019fca3777f8a2b3921aaa403
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-11-15 14:05:43 +0100

    Tested BookService

--- src/test/java/com/przemyslawwolosz/module/books/service/BookServiceTest.java
+++ src/test/java/com/przemyslawwolosz/module/books/service/BookServiceTest.java
@@ -6,60 +6,77 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookServiceTest {
 
-	private static final long VALID_BOOK_ID = 1;
-	private static final long NOT_VALID_BOOK_ID = 0;
-	private static final BookEntity BOOK = new BookEntity();
+    private static final long VALID_BOOK_ID = 1;
+    private static final long NOT_VALID_BOOK_ID = 0;
+    private static final BookEntity BOOK = new BookEntity();
 
-	@Mock
-	private BookRepository bookRepository;
+    @Mock
+    private BookRepository bookRepository;
 
-	@InjectMocks
-	private BookService bookService;
+    @InjectMocks
+    private BookService bookService;
 
-	@Test
-	public void shouldCallGetOneWhenIdIsValid() {
-		// when
-		// then
-	}
+    @Test // getOne(Long id)
+    public void shouldCallGetOneWhenIdIsValid() {
+        // when
+        bookService.getOne(VALID_BOOK_ID);
+        // then
+        Mockito.verify(bookRepository).getOne(VALID_BOOK_ID);
+    }
 
-	@Test
-	public void shouldNotCallGetOneWhenIdIsValid() {
-		// when
-		// then
-	}
+    @Test // getOne(Long id)
+    public void shouldNotCallGetOneWhenIdIsValid() {
+        // when
+        bookService.getOne(NOT_VALID_BOOK_ID);
+        // then
+        Mockito.verify(bookRepository, Mockito.times(0))
+                .getOne(NOT_VALID_BOOK_ID);
+    }
 
-	@Test
-	public void shouldCallFindAll() {
-		// when
-		// then
-	}
+    @Test // getAll()
+    public void shouldCallFindAll() {
+        // when
+        bookService.getAll();
+        // then
+        Mockito.verify(bookRepository).findAll();
+    }
 
-	@Test
-	public void shouldCallSaveAndFlash() {
-		// when
-		// then
-	}
+    @Test // create(BookEntity entity)
+    public void shouldCallSaveAndFlash() {
+        // when
+        bookService.create(BOOK);
+        // then
+        Mockito.verify(bookRepository).saveAndFlush(BOOK);
+    }
 
-	@Test
-	public void shouldNotCallSaveAndFlashWhenObjectIsEqualToNull() {
-		// when
-		// then
-	}
+    @Test // create(BookEntity entity)
+    public void shouldNotCallSaveAndFlashWhenObjectIsEqualToNull() {
+        // when
+        bookService.create(null);
+        // then
+        Mockito.verify(bookRepository, Mockito.times(0))
+                .saveAndFlush(BOOK);
+    }
 
-	@Test
-	public void shouldCallDeleteById() {
-		// when
-		// then
-	}
+    @Test // delete(Long id)
+    public void shouldCallDeleteById() {
+        // when
+        bookService.delete(VALID_BOOK_ID);
+        // then
+        Mockito.verify(bookRepository).deleteById(VALID_BOOK_ID);
+    }
 
-	@Test
-	public void shouldSaveAndFlashInCaseOfUpdate() {
-		// when
-		// then
-	}
+    @Test // update(Long id, BookEntity entity)
+    public void shouldSaveAndFlashInCaseOfUpdate() {
+        // when
+        bookService.update(VALID_BOOK_ID, BOOK);
+        // then
+        Mockito.verify(bookRepository).saveAndFlush(BOOK.setId(VALID_BOOK_ID));
+    }
 }
