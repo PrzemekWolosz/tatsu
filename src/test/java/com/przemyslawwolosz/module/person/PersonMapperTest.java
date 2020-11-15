607e97a949b35f79180ff3a40cd98b8a208c7b49
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-11-15 12:32:37 +0100

    Tested PersonMapper

--- src/test/java/com/przemyslawwolosz/module/person/PersonMapperTest.java
+++ src/test/java/com/przemyslawwolosz/module/person/PersonMapperTest.java
@@ -2,47 +2,78 @@ package com.przemyslawwolosz.module.person;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 public class PersonMapperTest {
 
-	private static final String NAME = "Michał";
-	private static final String SURNAME = "Szałkowski";
-	private static final String EMAIL = "no@michalszalkowski.com";
+    private static final String NAME = "Michał";
+    private static final String SURNAME = "Szałkowski";
+    private static final String EMAIL = "no@michalszalkowski.com";
 
-	@Test
-	public void shouldReturnNull() {
-		// given
-		// when
-		// then
+    @Test
+    public void shouldReturnNull() {
+        // given
+        PersonEntity personEntity = null;
+        // when
+        PersonDto personDto = PersonMapper.map(personEntity);
+        // then
+        assertThat(personDto).isNull();
 
-	}
+    }
 
-	@Test
-	public void shouldReturnDto() {
-		// given
-		// when
-		// then
-	}
+    @Test
+    public void shouldReturnDto() {
+        // given
+        PersonEntity personEntity = new PersonEntity()
+                .setName(NAME)
+                .setSurname(SURNAME)
+                .setEmail(EMAIL);
+        // when
+        PersonDto personDto = PersonMapper.map(personEntity);
+        // then
+        assertThat(personDto.getName()).isEqualTo(NAME);
+        assertThat(personDto.getSurname()).isEqualTo(SURNAME);
+        assertThat(personDto.getEmail()).isEqualTo(EMAIL);
+        assertThat(personDto.isEmailValid()).isTrue();
+    }
 
-	@Test
-	public void shouldReturnEmptyList() {
-		// given
-		// when
-		// then
-	}
+    @Test
+    public void shouldReturnEmptyList() {
+        // given
+        List<PersonEntity> persons = List.of();
+        // when
+        List<PersonDto> personDtos = PersonMapper.asList(persons);
+        // then
+        assertThat(personDtos).isEmpty();
+    }
 
-	@Test
-	public void shouldReturnList() {
-		// given
-		// when
-		// then
-	}
+    @Test
+    public void shouldReturnList() {
+        // given
+        List<PersonEntity> persons = List.of(
+                new PersonEntity().setName(NAME).setSurname(SURNAME).setEmail(EMAIL),
+                new PersonEntity().setName(NAME).setSurname(SURNAME).setEmail(EMAIL)
+        );
+        // when
+        List<PersonDto> personDtos = PersonMapper.asList(persons);
+        // then
+        assertThat(personDtos).isNotEmpty().hasSize(2);
+    }
 
-	@Test
-	public void shouldReturnSet() {
-		// given
-		// when
-		// then
-	}
+    @Test
+    public void shouldReturnSet() {
+        // given
+        List<PersonEntity> persons = List.of(
+                new PersonEntity().setName(NAME).setSurname(SURNAME).setEmail(EMAIL),
+                new PersonEntity().setName(NAME).setSurname(SURNAME).setEmail(EMAIL)
+        );
+        // when
+        Set<PersonDto> personDtos = PersonMapper.asSet(persons);
+        // then
+        assertThat(personDtos).isNotEmpty().hasSize(1);
+    }
 }
